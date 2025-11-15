a96402507771dbf9073214e637fa60213b8be4c5
Heyson315 <hassan@hhr-cpa.us>
2025-11-14 20:55:16 -0500

    Add project files.

---
+++ qwe/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+using System.Web.Mvc;
+
+namespace qwe.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Services()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ qwe/Models/Service.cs
@@ -0,0 +1,8 @@
+namespace qwe.Models
+{
+    public class Service
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
